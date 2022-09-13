9539c13c8cd8fad75714733291c589483cb98914
roma <roma.klabukov@gmail.com>
2022-09-13 09:53:05 +0300

    polish

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,18 +6,18 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract Token is ERC20, AccessControl {
-    bytes32 public constant MINTER_BURNER_ROLE =
-        keccak256("MINTER_BURNER_ROLE");
+    bytes32 public constant MINTER_ROLE =
+        keccak256("MINTER_ROLE");
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(MINTER_BURNER_ROLE, msg.sender);
-        _setRoleAdmin(MINTER_BURNER_ROLE, DEFAULT_ADMIN_ROLE);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _setRoleAdmin(MINTER_ROLE, DEFAULT_ADMIN_ROLE);
     }
 
     function mint(address to, uint256 amount)
         external
-        onlyRole(MINTER_BURNER_ROLE)
+        onlyRole(MINTER_ROLE)
     {
         _mint(to, amount);
     }
